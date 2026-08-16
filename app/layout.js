2163d3c60980b16eb26b34c76fdfebf484a0eafc
x47-cmd <x_47@hotmail.com>
2026-08-16 15:35:22 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><LanguageProvider>
+  {children}
+</LanguageProvider></body>
     </html>
   );
 }

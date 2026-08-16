77e1d723de10024e0bbd1c0489a064c984a28e86
x47-cmd <x_47@hotmail.com>
2026-08-16 15:34:36 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import "./globals.css";
 
+import {
+  LanguageProvider,
+} from "./components/LanguageProvider";
+
 export const metadata = {
   title: "AI Identity Reconciliation Platform",
   description:
